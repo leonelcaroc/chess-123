fa9b2d8c6b82348e7058aaf73337c02323f5ee17
leonelcaroc25@gmail.com <leonelcaroc25@gmail.com>
2022-03-28 14:45:41 +0800

    edited

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-Javascript
+const ele = document.getElementsByClassName("tables_container");
+ele.scrollTop = 100;
+ele.scrollLeft = 150;
+
+let pos = { top: 0, left: 0, x: 0, y: 0 };
+
+const mouseDownHandler = function(e) {
+  pos = {
+    // The current scroll
+    left: ele.scrollLeft,
+    top: ele.scrollTop,
+    // Get the current mouse position
+    x: e.clientX,
+    y: e.clientY,
+  };
+
+  document.addEventListener('mousemove', mouseMoveHandler);
+  document.addEventListener('mouseup', mouseUpHandler);
+};
+
